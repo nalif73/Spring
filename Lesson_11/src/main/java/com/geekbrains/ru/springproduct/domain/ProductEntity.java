9c596dcc2016c70e023cfa17ecb1716a25a46aca
Nalif <nalif@mail.ru>
2021-09-13 19:41:43 +0300

    Домашнее задание к 11 уроку

---
+++ Lesson_11/src/main/java/com/geekbrains/ru/springproduct/domain/ProductEntity.java
@@ -0,0 +1,44 @@
+package com.geekbrains.ru.springproduct.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product")
+
+
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="price")
+    private double price;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "categories_id")
+    @JsonIgnore
+    private CategoryEntity productCategory;
+
+    @Column(name="image_link")
+    private String imageLink;
+
+}
